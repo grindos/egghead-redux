413a74e54bdfd2f4e7f79e8984c57676daa87f42
grindos <grindosart@gmail.com>
2018-09-04 14:51:27 +0300

    Add router

--- src/components/Root/index.js
+++ src/components/Root/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Router, Route } from 'react-router';
+import createBrowserHistory from 'history/createBrowserHistory';
 import { Provider } from 'react-redux';
 import TodoApp from './TodoApp';
 
+const history = createBrowserHistory();
+
 const Root = ({ store }) => (
   <Provider store={store}>
-    <TodoApp />
+    <Router history={history}>
+      <Route path="/" component={TodoApp} />
+    </Router>
   </Provider>
 );
 Root.propTypes = {
